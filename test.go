8fca05c2af2ecf8d3cd90d49b77f107e6f9e0706
zhongjianzhen <523135169@qq.com>
2022-03-29 10:22:25 +0800

    99

--- test.go
+++ test.go
@@ -7,13 +7,9 @@ import (
 func main() {
 	fmt.Println("ok")
 }
-<<<<<<< HEAD
-
 func A() {
 	fmt.Println("第一个版本")
 }
 func B() {
 	fmt.Println("第二个版本")
 }
-=======
->>>>>>> parent of bf3e7ed (第一个版本)
